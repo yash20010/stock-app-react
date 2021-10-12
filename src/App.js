4272672f17e8ae5e644594ef5767de3da8c01abc
throne.yash@gmail.com <throne.yash@gmail.com>
2021-10-11 22:35:05 -0600

    Fetching coin data using axios in useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=CAD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res => {
+        console.log(res)
+      })
+  })
+
   return (
     <div className="App">
       <h1>APIIII</h1>
