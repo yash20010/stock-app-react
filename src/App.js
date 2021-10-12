a577d5e230be1a873289497fd8f0ae22d72c4a1f
throne.yash@gmail.com <throne.yash@gmail.com>
2021-10-11 22:36:49 -0600

    Adding the fetched coin to coins using useState

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import axios from 'axios'
 
 function App() {
 
+  const [coins, setCoins] = useState([])
+
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=CAD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
       .then(res => {
-        console.log(res)
+        setCoins(res.data)
+        console.log(coins)
       })
   })
 
