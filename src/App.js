7997bbcfca298e46d5992da399b513e675316f22
throne.yash@gmail.com <throne.yash@gmail.com>
2021-10-12 22:41:48 -0600

    fetching data only on load

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React, {useState, useEffect} from 'react'
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 import axios from 'axios'
 
 function App() {
-
   const [coins, setCoins] = useState([])
 
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=CAD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-      .then(res => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=CAD&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .then((res) => {
         setCoins(res.data)
         console.log(coins)
       })
-  })
+      .catch((err) => console.log(err))
+  }, [])
 
   return (
     <div className="App">
       <h1>APIIII</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
