02c04f89482e7023ce67f582069127e0a29cca1c
throne.yash@gmail.com <throne.yash@gmail.com>
2021-10-13 22:48:09 -0600

    adding Coin component

---
+++ src/Components/Coin.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Coin = () => {
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <img src={image} alt="crypto" />
+          <h1>{name}</h1>
+          <p className="coin-symbol">{symbol}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Coin
