343193b3f66e24fc6d1abc764dc6d167697469d1
throne.yash@gmail.com <throne.yash@gmail.com>
2021-10-15 23:04:35 -0600

    Passing props and catching them in Coins

--- src/App.js
+++ src/App.js
@@ -47,9 +47,10 @@ function App() {
             name={coin.name}
             image={coin.image}
             symbol={coin.symbol}
-            volume={coin.market_cap}
+            volume={coin.total_volume}
             price={coin.current_price}
             priceChange={coin.price_change_percentage_24h}
+            marketcap={coin.market_cap}
           />
         )
       })}
